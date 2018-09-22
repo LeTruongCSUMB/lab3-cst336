093cdd03b13f80dc2b95365c5c46b1f120b90d06
Noah Tolentino Nguyen <ntolentino-nguyen@csumb.edu>
2018-09-22 20:20:47 +0000

    Modify play function

--- inc/functions.php
+++ inc/functions.php
@@ -13,17 +13,23 @@
          
                 for($j = 1; $j <= 52; $j++) {
                     
-                    if(j < 14)
-                        $cards[j]["suit"] = "clubs";
-                    else if(j < 27)
-                        $cards[j]["suit"] = "diamonds";
-                    else if(j < 40)
-                        $cards[j]["suit"] = "hearts";
-                    else
-                        $cards[j]["suit"] = "hearts";
+                    if($j < 14) {
+                        $cards[$j]["suit"] = "clubs";
+                        $cards[$j]["value"] = $j;
+                        }
+                    else if($j < 27) {
+                        $cards[$j]["suit"] = "diamonds";
+                        $cards[$j]["value"] = $j-13;
+                    }
+                    else if($j < 40) {
+                        $cards[$j]["suit"] = "hearts";
+                        $cards[$j]["value"] = $j-26;
+                    }
+                    else {
+                        $cards[$j]["suit"] = "spades";
+                        $cards[$j]["value"] = $j-39;
+                    }
                         
-                    $cards[j]["value"] = j;
-                    $cards[j]["taken"] = 0;
                 }
         
     
@@ -34,7 +40,8 @@
         // Player are randomly seated
         for($i = 0; $i < 4; $i++) {
             $randNum = rand(0, $count($playerNames)-1);
-            $players[i]["name"] = $playerNames[$randNum];
+            $players[$i]["name"] = $playerNames[$randNum];
+            $players[$i]["score"] = 0;
             unset($playerNames[$randNum]);
         }
         return true;
@@ -42,9 +49,16 @@
     
     ///////////////////////////////////////////// Play
     function play () {
-        for ($j = 1; $j <= 4; $j++){
-            if($j == 1) {
+        for ($i = 0; $i < 4; $i++){
+            
+            while($players[$i]["score"] < 38) {
+                $randNum = rand(1, count($cards)-1);
+                // echo img with something like (yes this should be possible, but you have to adjust the quotes to be correct)
+                // href= "cards/$cards[$randNum]["suit"]/$cards[$randNum]["value"].png"
+                $players[$i]["score"] += $cards[$randNum]["value"];
+                unset($cards[$randNum]);
             }
+            
         }
         return true;
     }
