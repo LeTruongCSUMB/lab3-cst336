823f9b4c223687651ff8a12b2fa1be9ac91b087c
Noah Tolentino Nguyen <ntolentino-nguyen@csumb.edu>
2018-09-25 04:39:42 +0000

    Add CSS

--- inc/functions.php
+++ inc/functions.php
@@ -51,59 +51,89 @@
     ///////////////////////////////////////////// Play
     function play () {
         global $cards, $players;
-
+        echo "<div class='main'>";
+        echo "<header>Ultimate Cat Silverjack</header>";
         for ($i = 0; $i < 4; $i++){
-            
-            echo '<img src= "img/' . $players[$i]["name"] . ".jpg" . '" width=100px height=100px />';
+            echo "<div class='player'>";
+            echo '<img class=image src= "img/' . $players[$i]["name"] . ".jpg" . '" width=100px height=100px />';
+            echo "<span class='name'>". $players[$i]['name'] . "</span>";
+            echo "</div>";
             while($players[$i]["score"] < 38) {
                 
-                echo '<img src= "cards/' . $cards[0]['suit'] . "/" . $cards[0]["value"] . ".png" . '"/>';
+                echo '<img class="card" src= "cards/' . $cards[0]['suit'] . "/" . $cards[0]["value"] . ".png" . '"/>';
                 $players[$i]["score"] += $cards[0]["value"];
                 
                 unset($cards[0]);
                 $cards = array_values($cards);
             }
-            echo $players[$i]["score"];
+            echo "<div class='score'> ".$players[$i]["score"]. " Pts.</div>";
             echo "<br>";
-            echo $players[$i]["name"];
+           
+          
             echo "<br>";
         }
+
     }
     
     ///////////////////////////////////////////// Score
     function score() {
-        global $cards, $players;
-        $closest;
-        for ($i = 0; $i < 4; $i++){
-            echo $players[$i]['name'];
-            echo "<br><br>";
-            if ($players[$i]["score"] == 42) {
-                echo "Player ". $players[$i]['name']. " has won!";
-                echo $players[$i]["score"];
-                echo "<br><br>";
+        global $cards, $players, $starttime;
+        $winners = array();
+        
+        for($i = 42; $i >= 38; $i--) {
+            for($j = 0; $j < 4; $j++)
+                if($players[$j]["score"]==$i)
+                    $winners[] = $j;
+        
+            if(count($winners) > 0)
                 break;
-            }else {
-                for ($k = 1; $k <= 42; $k++) {
-                    $closest = $players[$i]["score"] - $k;
-                    if ($closest == 42) {
-                        echo "Player ". $players[$i]["name"] . " has won!";
-                        echo $players[$i]["score"];
-                        echo "<br><br>";
-                        break 2;
-                    }else if ($closest == 0) {
-                        echo "Player ". $players[$i]["name"] . " has won!";
-                        echo $players[$i]["score"];
-                        echo "<br><br>";
-                        break 2;
-                    }
+        }
+
+        for($i = 0; $i < count($winners); $i++) {
+            $players[$winners[$i]]["score"] = 0;
+            for($j = 0; $j < 4; $j++) {
+                if(!in_array($j, $winners)) {
+                    $players[$winners[$i]]["score"] += $players[$j]["score"];
                 }
             }
+            
+            echo "<div class='winner'>" . $players[$winners[$i]]["name"] . " has won " 
+                    . $players[$winners[$i]]["score"] . " points!" . "</div>";
+            echo "<br>";
         }
-        echo "End of for Loop";
-        echo "<br><br>";
+        
+        if(count($winners)==0)
+            echo "<div class='winner'>Everyone Lost!</div>";
+            
+        $endtime = microtime(true);
+        $timediff = $endtime - $starttime;
+        echo "<div class='end'>$timediff seconds have elapsed</div>";   
+        
+        session_start();
+
+        if (!isset($_SESSION['visited'])) {
+            
+            $_SESSION['visited'] = TRUE;
+            $_SESSION['totalTime'] = 0;
+            $_SESSION['counter'] = 0;
+        }
+        $_SESSION['counter']++;
+        $_SESSION['totalTime'] += $timediff;
+        
+        echo"<div class='end'>The average time elapsed is ". $_SESSION['totalTime']/$_SESSION['counter'] . " seconds</div>";
+        
+        $word = " time";
+        if($_SESSION['counter'] > 1)
+            $word = " times";
+            
+        echo "<div class='end'>This page has been visited ". $_SESSION['counter'] . "$word </div>";
+        echo "</div>";
+      
+
+
     }
     
- 
+
     
   
 
--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 
-<?php
-    include "inc/functions.php";
-?>
+<?php include "inc/functions.php"; ?>
 
 
 <html>
     <head>
         <meta charset="utf-8"/>
+        <link href="css/styles.css"  rel="stylesheet" />
         <title>Silverjack</title>
     </head>
     <body>
+           
             <?php
+                $starttime = microtime(true);
                 setupCards();
                 seatPlayers();
                 play();
                 score();
+                
             ?>
     </body>
 </html>
